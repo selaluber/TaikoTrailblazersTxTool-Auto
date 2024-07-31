929fa76c40d92be6e8398e552700470f445307e5
selaluber <selaluber@gmail.com>
2024-07-31 20:00:56 +0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+const config = {
+  totalLoop: 15,
+  loop: 10,
+  walletAddress: "your wallet",
+  privateKey: "your privat key",
+  contractAddress: "0xa530dc476eb4859C128a647a7e1e2fd39a5459ee", // WETH contract
+  rpcUrl: "https://rpc.mainnet.taiko.xyz",
+  etherAmount: "0.00001",
+};
+module.exports = config;
